5615e5c1f3d9de93effdf447c876ff62dddb68f7
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-07-10 15:10:59 +0800

    filter and map test:add 2 file in filter and 2 file in map

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,8 +1,11 @@
 'use strict';
 
 function choose_even(collection) {
-
   //implement here
+ /* const condition=()=>{
+    return  this[i]%2==0;
+  }*/
+  return collection.filter((item)=>item%2==0);
 }
 
 module.exports = choose_even;
--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -3,6 +3,10 @@
 function choose_multiples_of_three(collection) {
 
   //implement here
+  /*const condition=()=>{
+    return  this[i]%3==0;
+  }*/
+  return collection.filter((item)=>item%3==0);
 }
 
 module.exports = choose_multiples_of_three;
