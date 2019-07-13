83f0dd0d14471a86f0de469322e89964aec234ee
DARYL HUANG (ITA-ISDC-ISD-OOCLL/ZHA) <HUANGDA7@OOCL.COM>
2019-07-13 16:04:20 +0800

    feature:complete grouping_counting.js in practice filter

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -1,8 +1,25 @@
 'use strict';
-
+const callback=(a,c,i)=>{
+  if(Object.getOwnPropertyNames(a).length==0)
+  a[c]=1;
+  else{
+    let flag=0;
+    for(let att in a){
+      if(c==att){
+		    a[att]++;
+        flag=1;
+        break;
+      }
+    }
+    if(flag==0)
+    a[c]=1;
+  }
+  return a;
+}
 function grouping_count(collection) {
 
   //implement here
+  return collection.reduce(callback,{});
 }
 
 module.exports = grouping_count;
